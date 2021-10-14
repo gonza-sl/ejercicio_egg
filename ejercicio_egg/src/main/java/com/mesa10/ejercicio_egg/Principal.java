18ba65bb7edb2456be2e20b205fa4d3ec0331d38
gonza <g@gnet>
2021-10-14 20:41:29 -0300

    primer commit con codigo

---
+++ ejercicio_egg/src/main/java/com/mesa10/ejercicio_egg/Principal.java
@@ -0,0 +1,20 @@
+
+package com.mesa10.ejercicio_egg;
+
+import com.mesa10.ejercicio_egg.entidades.Gato;
+
+/**
+ *
+ * @author gonza
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Gato g = new Gato(1, "Lola", "Común");
+        System.out.println(g);
+    }
+    
+}
