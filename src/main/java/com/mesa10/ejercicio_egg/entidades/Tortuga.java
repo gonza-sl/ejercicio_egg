60bf3f4487ae0d8f355863c0bad5492803fed00a
gonza <g@gnet>
2021-10-15 00:20:16 -0300

    primer commit de tortuta

---
+++ src/main/java/com/mesa10/ejercicio_egg/entidades/Tortuga.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mesa10.ejercicio_egg.entidades;
+
+/**
+ *
+ * @author gonza
+ */
+public class Tortuga {
+    private Long id;
+    private String name;
+}
